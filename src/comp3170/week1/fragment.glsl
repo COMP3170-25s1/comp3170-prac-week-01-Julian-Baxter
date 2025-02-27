97e7f46dbdd77418aa496a86ce69e7e7351fbb6a
Julian-Baxter <69179532+Julian-Baxter@users.noreply.github.com>
2025-02-27 14:56:49 +1100

    Week 1 Change 2
    I have looked into the code, and learned about how the process works.

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -6,9 +6,9 @@ uniform vec2 u_screenSize;  	// screen dimensions in pixels
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
-   vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
+   vec2 p = (gl_FragCoord.xy / u_screenSize);   // scale p into range (0,0) to (1,1)
+	vec2 v = abs(p - vec2(0.5, 0.5));
+	float d = max(v.x*2, v.y*2);       
    if (d < 0.5) {
       o_colour = vec4(u_colour, 1);
    }
